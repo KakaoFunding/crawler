c18ef8f59c026a40c6172d58b06e0fb29888f280
chestnut1044 <wbk1620@gmail.com>
2024-03-19 21:02:12 +0900

    [feat]: 카테고리 정보 크롤링 (#1)

--- index.py
+++ index.py
@@ -1,5 +1,12 @@
+from service.readCategoryData import getMainCategoryInfos, getSubCategoryInfos
 from service.readBrandData import getBrandInfos
 from service.readProductdata import getProductInfo
+
+# 부모 카테고리 정보
+getMainCategoryInfos()
+
+# 자식 카테고리 정보
+getSubCategoryInfos()
 
 # 브랜드 정보
 getBrandInfos()
